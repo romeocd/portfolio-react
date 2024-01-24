0f4c0c419ca32190fb504fcc87af4551329e006a
romeo <rdumlao07@gmail.com>
2024-01-23 19:38:19 -0500

    create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='footer-content'>
+            <a href="https://github.com/romeocd" target="_blank" rel="noopener noreferrer">GitHub</a>
+            <a href="https://www.linkedin.com/in/romeo-dumlao-jr-59164216a/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a href="https://www.instagram.com/romeo_dumlao/" target="_blank" rel="noopener noreferrer">Instagram</a>
+        </div>
+    );
+};
