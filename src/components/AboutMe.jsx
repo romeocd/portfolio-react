2a1d908643f9c12032b4d81b10890afdfc3286b8
romeo <rdumlao07@gmail.com>
2024-01-25 14:06:11 -0500

    edit about me section styling and more nav styling

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const AboutMe = () => {
   return (
     <div className="about-me">
-      <img src="/path/to/your/avatar.jpg" alt="Your Name" />
+      <img src="./src/assets/images/profilepic.jpg" alt="Romeo Dumlao Jr." className="profile-picture" />
       <div className="bio">
         <h2>About Me</h2>
         <p>
-          Write a short bio here. This might include your background, 
-          what you're passionate about, your skills, or any other 
-          information you want to share about yourself.
+        Hello! I'm Romeo Dumlao and welcome to my personal portfolio! I am a beginner web developer and currently learning to be a Full Stack Web Developer under the Rutgers Coding Bootcamp program.
+I will be making updates to this page as I learn and grow as a developer. Please click on my Portfolio to see my projects so far.
+If you have any questions, please don't hesitate to contact me!
         </p>
         {/* Add more personal details or sections as needed */}
       </div>
