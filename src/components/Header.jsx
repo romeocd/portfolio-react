b01244cbb947366921b8e37a4eb01a9162d88258
romeo <rdumlao07@gmail.com>
2024-01-25 13:48:07 -0500

    edit header and footer styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Navigation from './Navigation'
 
 const Header = () => {
     return (
-        <header>
+        <header className="header">
             <h1>Romeo Dumlao Jr.</h1>
             <Navigation />
         </header>
